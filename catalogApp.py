cd8d12da2ffe537ea964315c0364c7be889aeaa7
Rumman <abc@123.com>
2019-03-09 01:02:49 -0500

    fixed google sign in

--- catalogApp.py
+++ catalogApp.py
@@ -143,52 +143,26 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     # Obtain authorization code
-    code = request.data
+    print("obtaining id_token...")
+    idToken = request.data
 
-    print("Code: " + code)
+    print("Code: " + idToken)
 
-    try:
-        # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
-        oauth_flow.redirect_uri = 'postmessage'
-        credentials = oauth_flow.step2_exchange(code)
-        # print(credentials.to_json())
-    except FlowExchangeError:
-        response = make_response(
-            json.dumps('Failed to upgrade the authorization code.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    # Check that the access token is valid.
-    access_token = credentials.access_token
-    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
-           % access_token)
+    print("sending req to tokeninfo...")
+    url = ('https://oauth2.googleapis.com/tokeninfo?id_token=%s'
+           % idToken)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
-    # If there was an error in the access token info, abort.
-    if result.get('error') is not None:
-        response = make_response(json.dumps(result.get('error')), 500)
-        response.headers['Content-Type'] = 'application/json'
-        return response
+    print(result)
 
-    # Verify that the access token is used for the intended user.
-    gplus_id = credentials.id_token['sub']
-    print("token gplus_id: %s" % gplus_id)
-
-    if result['user_id'] != gplus_id:
-        response = make_response(
-            json.dumps("Token's user ID doesn't match given user ID."), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    # Verify that the access token is valid for this app.
-    if result['issued_to'] != CLIENT_ID:
+    gplus_id = result['sub']
+    print("verifying aud matches clientID...")
+    if result['aud'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
         print("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
-
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
@@ -198,31 +172,22 @@ def gconnect():
         return response
 
     # Store the access token in the session for later use.
-    login_session['access_token'] = credentials.access_token
-    login_session['gplus_id'] = gplus_id
+    login_session['access_token'] = idToken
+    login_session['gplus_id'] = result['sub']
 
-    # Get user info
-    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
-    params = {'access_token': credentials.access_token, 'alt': 'json'}
-    answer = requests.get(userinfo_url, params=params)
-    data = answer.json()
-
-    # print("data:\n")
-    # print(data)
-
-    if "name" in data:
-        login_session['username'] = data['name']
+    if "name" in result:
+        login_session['username'] = result['name']
     else:
-        login_session['username'] = data['email']
-    login_session['picture'] = data['picture']
-    login_session['email'] = data['email']
+        login_session['username'] = result['email']
+    login_session['picture'] = result['picture']
+    login_session['email'] = result['email']
 
     # ADD PROVIDER TO LOGIN SESSION
     login_session['provider'] = 'google'
 
     # Check to see if user exists in the DB. Create one if does not exist
     # see if user exists
-    user_id = getUserID(data['email'])
+    user_id = getUserID(result['email'])
     if not user_id:
         user_id = createUser(login_session)
     login_session['user_id'] = user_id
